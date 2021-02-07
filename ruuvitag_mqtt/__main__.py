06117b7cf63ab9fac2fa8090e3aeaf4d0769bcf3
Jaakko Vuori <jaakko.vuori@gmail.com>
2021-02-07 19:04:52 +0200

    Simple configuration system

--- ruuvitag_mqtt/__main__.py
+++ ruuvitag_mqtt/__main__.py
@@ -1,39 +1,58 @@
-from paho.mqtt import client
+import argparse
+from pathlib import Path
+import json
+import logging
+
+from paho.mqtt import client as mqtt
+from paho.mqtt.properties import Properties, PacketTypes
 from ruuvitag_sensor.ruuvi import RuuviTagSensor
 
+logging.basicConfig(level=logging.DEBUG)
 
-mqtt_client = client.Client("RuuviTag")
-mqtt_client.connect(
-    "192.168.1.11",
-)
-mqtt_client.disconnect()
+logger = logging.getLogger(__name__)
 
-mac_location_map = {
-    "DE:D4:96:2C:3C:78": "kitchen",
-    "E3:35:52:F1:D2:AA": "terrace",
-    "F9:89:71:39:A5:82": "sauna",
-}
-
-valid_keys = [
-    "temperature",
-    "humidity",
-]
+mqtt_client = mqtt.Client("RuuviTag")
 
 
 def on_ruuvi_event(ruuvi_event):
     mac_address, data = ruuvi_event
-    location = mac_location_map.get(mac_address)
-    if location:
-        mqtt_client.reconnect()
-        for key, value in data.items():
-            if key in valid_keys:
-                mqtt_client.publish(f"home/{location}/{key}", value, retain=True)
-        mqtt_client.disconnect()
+    configured_ruuvitags = config.get("ruuvitags", {})
+    location = configured_ruuvitags.get(mac_address, {}).get("name", mac_address)
+
+    mqtt_client.reconnect()
+    for key, value in data.items():
+        fields = configured_ruuvitags.get(mac_address, {}).get("fields")
+        if fields is None or key in fields:
+            mqtt_client.publish(
+                f"home/{location}/{key}",
+                value,
+            )
+    mqtt_client.disconnect()
 
 
-def start_publishing():
+def start_publishing(config_file_path: Path):
+    global config
+
+    logger.info("Using config file: %s", config_file_path)
+
+    if not config_file_path:
+        config_file_path = Path(__file__).parent / "ruuvitag_mqtt.json"
+    with open(config_file_path) as config_file:
+        config = json.load(config_file)
+    print(config)
+
+    mqtt_client.connect(
+        host=config.get("broker", {}).get("host", "localhost"),
+        port=config.get("broker", {}).get("port", 1883),
+    )
+    mqtt_client.disconnect()
+
     RuuviTagSensor.get_datas(on_ruuvi_event)
 
 
 if __name__ == "__main__":
-    start_publishing()
+    argument_parser = argparse.ArgumentParser()
+    argument_parser.add_argument("-c", dest="config_file")
+    args = argument_parser.parse_args()
+
+    start_publishing(args.config_file)
